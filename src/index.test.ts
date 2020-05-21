7ffc8ff78cb67f540434f876721fcabccbf9c0c6
Alex Uskov <FrozenAlex@users.noreply.github.com>
2020-05-21 17:57:12 +0300

    Tests example

---
+++ src/index.test.ts
@@ -0,0 +1,9 @@
+
+describe("Sample tests", () => {
+    test("Example test 1", () => {
+        expect(1 + 1).toBe(2);
+    })
+    test("Example test 2", () => {
+        expect({ hello: "world" }).toMatchObject({ hello: "world" })
+    })
+})
